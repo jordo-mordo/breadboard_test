7083b80781c6d154cf79b7a297b3c77345afe3b2
Jordan Moore <jordanhmoore@gmail.com>
2022-07-31 16:46:22 -0400

    Functionality to add second part and index steps, keeping track of previous parts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 function App() {
@@ -23,17 +23,30 @@ function App() {
     const nextParts = [...parts];
     nextParts[step - 1] = partObj;
     setParts(nextParts);
+    let currentStep = step;
+    setStep(currentStep + 1);
+    setCurrentPart('');
   };
 
-  console.log(currentPart, parts);
+  const goBack = () => {
+    let currentStep = step;
+    setCurrentPart(JSON.stringify(parts[currentStep - 2]));
+    setStep(currentStep - 1);
+  }
+
+
+
+
+
+  console.log(step, parts);
   return (
     <Container>
       <StepTracker step={step} number={1}>1</StepTracker>
       <StepTracker step={step} number={2}>2</StepTracker>
       <StepTracker step={step} number={3}>3</StepTracker>
       <h1>Upload Parts</h1>
-      <InputBox onChange={textChange}></InputBox>
-      <button>Back</button>
+      <InputBox onChange={textChange} value={currentPart}></InputBox>
+      <button onClick={goBack}>Back</button>
       <button onClick={addPart}>Next</button>
     </Container>
   );
@@ -49,5 +62,5 @@ const InputBox = styled.textarea`
 `;
 
 const StepTracker = styled.div`
-  background: ${({step, number}) => step >= number ? 'green': 'white'};
+  background: ${({step, number}) => step > number ? 'green': 'white'};
 `;
